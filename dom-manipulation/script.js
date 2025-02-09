27972ae5c4c609562fa8edebf9586f5bb2c527e0
Shahd Hesham <160048572+Shahdhesham147@users.noreply.github.com>
2025-02-09 23:51:45 +0200

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -353,4 +353,15 @@ function addQuote() {
     document.getElementById("newQuoteCategory").value = "";
 }
 
+function showNotification(message, type = "success") {
+    const notification = document.createElement("div");
+    notification.className = `notification ${type}`;
+    notification.textContent = message;
+
+    document.body.appendChild(notification);
+
+    setTimeout(() => {
+        notification.remove();
+    }, 3000); // يتم إخفاء الإشعار بعد 3 ثوانٍ
+}
 
